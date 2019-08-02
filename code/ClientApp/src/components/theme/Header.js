58a865791e185bad80b7d02a7e464feee80e8080
Doug Parsons <Doug.Parsons@harley-davidson.com>
2019-08-02 15:29:38 -0400

    Initial check-in

---
+++ code/ClientApp/src/components/theme/Header.js
@@ -0,0 +1,117 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Grid from '@material-ui/core/Grid';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+import CountDown from '../widgets/CountDown';
+
+const lightColor = 'rgba(255, 255, 255, 0.7)';
+
+const styles = theme => ({
+    secondaryBar: {
+        zIndex: 0,
+    },
+    menuButton: {
+        marginLeft: -theme.spacing(1),
+    },
+    iconButtonAvatar: {
+        padding: 4,
+    },
+    link: {
+        textDecoration: 'none',
+        color: lightColor,
+        '&:hover': {
+            color: theme.palette.common.white,
+        },
+    },
+    button: {
+        borderColor: lightColor,
+    },
+});
+
+function Header(props) {
+    const { classes, onDrawerToggle } = props;
+
+    return (
+        <React.Fragment>
+            <AppBar color="primary" position="sticky" elevation={0}>
+                <Toolbar>
+                    <Grid container spacing={1} alignItems="center">
+                        <Hidden smUp>
+                            <Grid item>
+                                <IconButton
+                                    color="inherit"
+                                    aria-label="open drawer"
+                                    onClick={onDrawerToggle}
+                                    className={classes.menuButton}
+                                >
+                                    <MenuIcon />
+                                </IconButton>
+                            </Grid>
+                        </Hidden>
+                        <Grid item xs />
+                        <Grid item>
+                            <CountDown />
+                            <Typography className={classes.link} component="span">
+                                
+              </Typography>
+                        </Grid>
+                    </Grid>
+                </Toolbar>
+            </AppBar>
+            <AppBar
+                component="div"
+                className={classes.secondaryBar}
+                color="primary"
+                position="static"
+                elevation={0}
+            >
+                <Toolbar>
+                    <Grid container alignItems="center" spacing={1}>
+                        <Grid item xs>
+                            <Typography color="inherit" variant="h5" component="h1">
+                                Stats
+              </Typography>
+                        </Grid>
+                    </Grid>
+                </Toolbar>
+            </AppBar>
+            <AppBar
+                component="div"
+                className={classes.secondaryBar}
+                color="primary"
+                position="static"
+                elevation={0}>
+                <Toolbar>
+                    <Grid container alignItems="center" spacing={1}>
+                        <Grid item>
+                            <Typography color="inherit" component="span">
+                                1st: None | 2nd: None | 3rd: None | Total Team Points: 0
+                                [Control]
+              </Typography>
+                        </Grid>
+                        <Grid item xs />
+                        <Grid item>
+                            <Typography color="inherit" component="span">
+                                Rewards Active: Yes | No [Control]
+              </Typography>
+                        </Grid>
+                    </Grid>
+                </Toolbar>
+            </AppBar>
+        </React.Fragment>
+    );
+}
+
+Header.propTypes = {
+    classes: PropTypes.object.isRequired,
+    onDrawerToggle: PropTypes.func.isRequired,
+};
+
+export default withStyles(styles)(Header);
---
+++ code/ClientApp/src/components/widgets/BountyList.js
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+
+export default class BountyList extends React.Component {
+    _timer;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            bounties = props.bounties
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.bounties}
+            </div>
+        );
+    }
+}
